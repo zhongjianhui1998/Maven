9f00a9f79185afb82133151c9256af12c289872d
zhongjianhui1998 <59462608+zhongjianhui1998@users.noreply.github.com>
2020-01-04 21:57:43 +0800

    Update test.java
    dcsdws

--- src/test/test.java
+++ src/test/test.java
@@ -4,8 +4,12 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("test");
 		System.out.println("zhongguojunren");
-		 地方干部vdffd房地产 从vffgfrtfhbdfhbgfjnh
-			 更方便洗面奶 换个目标jh
-			 法定标准v
+	System.out.println("zhongguojunren");
+		System.out.println("zhongguojunren");
+		System.out.println("zhongguojunren");
+		System.out.println("zhongguojunren");
+		System.out.println("zhongguojunren");
+		System.out.println("zhongguojunren");
+		System.out.println("zhongguojunren");
 	}
 }
