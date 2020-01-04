d42d76a8ddd70812107d2cb3f1ab934351ef8962
zhongjianhui1998 <59462608+zhongjianhui1998@users.noreply.github.com>
2020-01-04 21:47:56 +0800

    Update test.java
    aaa

--- src/test/test.java
+++ src/test/test.java
@@ -4,5 +4,8 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("test");
 		System.out.println("zhongguojunren");
+		 地方干部vdffd房地产 从vffgfrtfhbdfhbgfjnh
+			 更方便洗面奶 换个目标jh
+			 法定标准v
 	}
 }
